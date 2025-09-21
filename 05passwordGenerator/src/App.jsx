eeec7873e220701a45db7f1c40edf96b712918e4
Sushan Karki <karkisush1470@gmail.com>
2025-09-21 15:48:17 +0545

    change

---
+++ 05passwordGenerator/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [length, setLength] = useState(8)
+  const [numberallowed, setNumberallowed] = useState(false)
+  const[character, setCharacter] = useState(false)
+  const [password, setPassword] = useState()
+
+  const passwordGenerator 
+
+
+  return (
+    <>
+    <h1 className='text-4xl text-center text-white'>Password Generator</h1>
+    </>
+  )
+}
+
+export default App
